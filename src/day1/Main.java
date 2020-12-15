cbb654405872249544676205eab975708ab8e979
ZHANGZHENG3210 <1874183210@qq.com>
2020-12-15 15:42:06 +0800

    idea配置github实验

--- src/day1/Main.java
+++ src/day1/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int[] a=new int[54];
         for(int i=0;i<54;i++){
-            a[i]=i+1;
+            a[i]=i+3;
         }
     }
 }
